ed6c09186a8048fa325c56c3fd85b3d9ff46acbf
jonghyokim <whdgy7@naver.com>
2019-08-20 17:43:03 +0900

    커밋

--- src/main/java/com/tobe/mes/tobesystem/Mapper/Scm/Partners/SCMDCList_Mapper.java
+++ src/main/java/com/tobe/mes/tobesystem/Mapper/Scm/Partners/SCMDCList_Mapper.java
@@ -1,4 +1,11 @@
 package com.tobe.mes.tobesystem.Mapper.Scm.Partners;
 
-public interface SCMDCList_Mapper {
+import com.tobe.mes.tobesystem.Bean.MESBean.SCM_DC.SCM_DC;
+import com.tobe.mes.tobesystem.Bean.MESBean.SCM_DC_BOX.SCM_DC_BOX;
+import com.tobe.mes.tobesystem.Bean.Page;
+import com.tobe.mes.tobesystem.Bean.Result;
+
+import java.util.List;
+
+public interface SCMDC_Mapper {
 }
